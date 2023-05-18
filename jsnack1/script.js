33bbbfd18bbeb8bfdb03557baa681d4e4faaf304
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-18 15:02:38 +0200

    tweaks

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,10 +2,10 @@ const storeNumbers = [];
 let sum = 0;
 
 for(let i = 0; i < 10; i++){
-    const numToInsert = parseInt(prompt(`Inserisci dieci numeri: `));
+    const numToInsert = parseInt(prompt(`Inserisci dieci numeri - ${i + 1}`));
     console.log(numToInsert);
     storeNumbers.push(numToInsert);
     sum = sum + numToInsert;
 }
 
-console.log('somma:' + sum);
+console.log('Somma totale - ' + sum);
