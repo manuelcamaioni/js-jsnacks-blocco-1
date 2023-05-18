bccb82de29f8fb854f1043f3faef8cd294fc0a3d
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-18 16:22:01 +0200

    added comments

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,12 +1,12 @@
 const numbers = prompt('Inserisci un valore.');
 
-const numbersList = numbers.split('');
+const numbersList = numbers.split(''); //* Scompone i caratteri della stringa inserita in elementi di un array
 
 let sum = 0;
 
 for (let i = 0; i < numbersList.length; i++){
-    const num = parseInt(numbersList[i]);
-    sum = sum + num;
+    const arrayElement = parseInt(numbersList[i]); //* Ogni elemento diventa un numero
+    sum = sum + arrayElement; //* calcolo la somma per ogni valore
 }
 
 console.log(`Somma: ${sum}`);
